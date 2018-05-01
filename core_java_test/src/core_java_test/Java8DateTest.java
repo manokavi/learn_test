9fe6379b0f3f9f42964ec1f71df7eee02a45bced
kaveen <kaveen@kaveen>
2018-05-01 22:49:14 +0530

    First java program for Java 8 date and time

---
+++ core_java_test/src/core_java_test/Java8DateTest.java
@@ -0,0 +1,14 @@
+package core_java_test;
+
+import java.time.LocalDate;
+
+public class Java8DateTest {
+
+	public static void main(String[] args) {
+		
+		LocalDate currentDate = LocalDate.now();
+		System.out.println(currentDate);
+
+	}
+
+}
