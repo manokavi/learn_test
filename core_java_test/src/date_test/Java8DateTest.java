4f8f7783607be78d2546ae676c78fbb13aab24ca
Manoj <vaiomanoj@gmail.com>
2020-01-05 18:56:39 +0530

    test

--- core_java_test/src/date_test/Java8DateTest.java
+++ core_java_test/src/date_test/Java8DateTest.java
@@ -7,7 +7,7 @@ public class Java8DateTest {
 	public static void main(String[] args) {
 		
 		LocalDate currentDate = LocalDate.now();
-		System.out.println("Current Date : " + currentDate);
+		System.out.println("Current Date Only : " + currentDate);
 
 	}
 
